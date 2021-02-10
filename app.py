69e13610a08e2e3454da6d108942e93c6657f8c1
Benitez, Anthony <anthony.benitezrodriguez@bbva.com>
2021-02-10 15:27:59 -0600

    added main

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-DB_HOST = "ec2-67-202-63-147.compute-1.amazonaws.com"
-DB_NAME = "d7j127qpvbukdo"
-DB_USER = "zvtclmedqrwqwd"
-DB_PASS = "ceea7532a0c5ea5d15d3fb1636be39bdabbeb4a34d3fece0a6c606d589d530a2"
-
 import flask
-from flask import request
 import psycopg2
 import psycopg2.extras
-import json
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
+    DB_HOST = "ec2-67-202-63-147.compute-1.amazonaws.com"
+    DB_NAME = "d7j127qpvbukdo"
+    DB_USER = "zvtclmedqrwqwd"
+    DB_PASS = "ceea7532a0c5ea5d15d3fb1636be39bdabbeb4a34d3fece0a6c606d589d530a2"
+
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT * FROM characters;")
@@ -27,3 +25,7 @@ def home():
     conn.close()
     all_characters = json_data
     return print(all_characters)
+
+
+if __name__ == '__main__':
+    app.run()
